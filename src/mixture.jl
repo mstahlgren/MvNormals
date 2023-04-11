ff2dc1e8d940f3ebf93008f650f9f03b6c23ee7d
Martin Ståhlgren <martin.stahlgren@gmail.com>
2023-04-12 00:53:40 +0200

    Renaming of abstract types

--- src/mixture.jl
+++ src/mixture.jl
@@ -9,6 +9,6 @@ end
 
 Base.:+(m::Mixture{D}, d::D) where {D <: MvNormal} = Mixture([dᵢ + d for dᵢ in m.dists])
 
-sample(m::Mixture, n::Int) = [sample(d, 1) for d in rand(m.dists, n)]
+StatsBase.:sample(m::Mixture, n::Int) = [sample(d, 1) for d in rand(m.dists, n)]
 
 logpdf(m::Mixture, o) = sum([logpdf(d, o) for d in m.dists]) / length(m.dists)
--- src/mvnormal.jl
+++ src/mvnormal.jl
@@ -1,66 +1,66 @@
 using LinearAlgebra: I, Hermitian, cholesky, logdet
 import StatsBase
 
-export MvNormal, ZeroMeanMvNormal, IsoMvNormal 
+export DenseMvNormal, DenseZeroMeanMvNormal, IsoMvNormal
 export logpdf, sample, mu, sigma, size
 
-abstract type AbstractMvNormal <: Distribution end
+abstract type MvNormal <: Distribution end
 
-abstract type AbstractZeroMeanMvNormal <: AbstractMvNormal end
+abstract type ZeroMeanMvNormal <: MvNormal end
 
-struct IsoMvNormal <: AbstractZeroMeanMvNormal
+struct IsoMvNormal <: ZeroMeanMvNormal
     n::Int64
 end
 
-struct ZeroMeanMvNormal <: AbstractZeroMeanMvNormal
+struct DenseZeroMeanMvNormal <: ZeroMeanMvNormal
     Σ::Matrix
 end
 
-struct MvNormal <: AbstractMvNormal
+struct DenseMvNormal <: MvNormal
     μ::Vector
     Σ::Matrix
  end
 
-Base.:size(x::AbstractMvNormal) = size(sigma(x), 1)
+Base.:size(x::MvNormal) = size(sigma(x), 1)
 
 Base.:size(x::IsoMvNormal) = x.n
 
-mu(x::AbstractMvNormal) = x.μ
+mu(x::MvNormal) = x.μ
 
 mu(x::ZeroMeanMvNormal) = x |> size |> zeros # Zero struct could be useful
 
-sigma(x::AbstractMvNormal) = x.Σ
+sigma(x::MvNormal) = x.Σ
 
 sigma(x::IsoMvNormal) = 1.0I(x |> size)
 
-Base.:+(d₁::AbstractMvNormal, d₂::AbstractMvNormal) = MvNormal(mu(d₁) .+ mu(d₂), sigma(d₁) .+ sigma(d₂))
+Base.:+(d₁::MvNormal, d₂::MvNormal) = MvNormal(mu(d₁) .+ mu(d₂), sigma(d₁) .+ sigma(d₂))
 
-Base.:+(d₁::AbstractZeroMeanMvNormal, d₂::AbstractMvNormal) = MvNormal(mu(d₂), sigma(d₁) .+ sigma(d₂))
+Base.:+(d₁::ZeroMeanMvNormal, d₂::MvNormal) = MvNormal(mu(d₂), sigma(d₁) .+ sigma(d₂))
 
-Base.:+(d₂::AbstractMvNormal, d₁::AbstractZeroMeanMvNormal) = d₁ + d₂
+Base.:+(d₁::MvNormal, d₂::ZeroMeanMvNormal) = d₂ + d₁ 
 
-Base.:+(d₁::AbstractZeroMeanMvNormal, d₂::AbstractZeroMeanMvNormal) = ZeroMeanMvNormal(sigma(d₁) .+ sigma(d₂))
+Base.:+(d₁::ZeroMeanMvNormal, d₂::ZeroMeanMvNormal) = ZeroMeanMvNormal(sigma(d₁) .+ sigma(d₂))
 
-Base.:+(v::Vector, d::AbstractMvNormal) = MvNormal(v .+ mu(d), sigma(d))
+Base.:+(v::Vector, d::MvNormal) = MvNormal(v .+ mu(d), sigma(d))
 
-Base.:+(v::Vector, d::AbstractZeroMeanMvNormal) = MvNormal(v, sigma(d))
+Base.:+(v::Vector, d::ZeroMeanMvNormal) = MvNormal(v, sigma(d))
 
-Base.:*(m::Matrix, d::AbstractMvNormal) = MvNormal(m*mu(d), m*sigma(d)*m')
+Base.:*(m::Matrix, d::MvNormal) = MvNormal(m*mu(d), m*sigma(d)*m')
 
-Base.:*(m::Matrix, d::AbstractZeroMeanMvNormal) = ZeroMeanMvNormal(m*sigma(d)*m')
+Base.:*(m::Matrix, d::ZeroMeanMvNormal) = ZeroMeanMvNormal(m*sigma(d)*m')
 
 Base.:*(m::Matrix, ::IsoMvNormal) = ZeroMeanMvNormal(m*m')
 
-Base.:(==)(x::AbstractMvNormal, y::AbstractMvNormal) = (mu(x) == mu(y)) && (sigma(x) == sigma(y))
+Base.:(==)(x::MvNormal, y::MvNormal) = (mu(x) == mu(y)) && (sigma(x) == sigma(y))
 
-function Base.:&(d₁::AbstractMvNormal, d₂::AbstractMvNormal)
+function Base.:&(d₁::MvNormal, d₂::MvNormal)
     c = (sigma(d₁) + sigma(d₂)) |> Hermitian |> cholesky
     Σ₁ = c.L\sigma(d₁); Σ₂ = c.L\sigma(d₂)
     μ₁ = c.L\mu(d₁); μ₂ = c.L\mu(d₂)
     return DenseMvNormal(Σ₂'*μ₁ .+ Σ₁'*μ₂, Σ₁'*Σ₂)
 end
 
-function logpdf(x::AbstractMvNormal, o::Vector)
+function logpdf(x::MvNormal, o::Vector)
     C = x |> sigma |> Hermitian |> cholesky
     ld = log(2π)*length(o) + 2*logdet(C.U)
     le = C\(o-x.μ)

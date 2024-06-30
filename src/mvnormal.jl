6f329ca47f0f8982c6547900c3e6d5f5f28a4462
Martin Ståhlgren <martin.stahlgren@gmail.com>
2024-07-01 01:34:04 +0300

    Refactored to utilize LLT decomposition directly

--- src/mvnormal.jl
+++ src/mvnormal.jl
@@ -1,117 +1,112 @@
-using LinearAlgebra: LowerTriangular, Symmetric, I, UniformScaling, Cholesky, cholesky, logdet, Diagonal, Hermitian
+import LinearAlgebra: UpperTriangular, Diagonal, Hermitian, Cholesky, cholesky, cholesky!, logdet
 import ChainRulesCore: ChainRulesCore, Tangent, NoTangent, ZeroTangent, ProjectTo, rrule, @thunk
+import Distributions: Distributions, ContinuousMultivariateDistribution, logpdf
 
-export MvNormal, IsoMvNormal, μ, Σ, logpdf, logpdfnan
-export product, product2
+export MvNormal, IsoMvNormal, μ, Σ, product, logpdf, logpdfnan
 
-struct Identity <: AbstractMatrix{Float64} end
-
-struct MvNormal2{V <: AbstractVector, M <: AbstractMatrix} 
+struct MvNormal{V, M} <: ContinuousMultivariateDistribution
     n::Int
     μ::V
-    L::M
+    U::M
 end
 
-const IsoMvNormal2 = MvNormal2{Zero, Identity}
+const IsoMvNormal = MvNormal{Zero, Identity}
 
-MvNormal2(n::Integer) = MvNormal2(n, Zero(), Identity())
+MvNormal(n::Integer) = MvNormal(n, Zero(), Identity())
 
-MvNormal2(μ, Σ::Matrix) = MvNormal2(length(μ), μ, cholesky(Σ).L)
+MvNormal(μ, Σ::Matrix) = MvNormal(length(μ), μ, cholesky(Σ).U)
 
-MvNormal2(μ, D::Diagonal) = MvNormal2(length(μ), μ, LowerTriangular(D))
+MvNormal(μ, U) = MvNormal(length(μ), μ, U)
 
-MvNormal2(μ, L) = MvNormal2(length(μ), μ, L)
+μ(x::MvNormal) = x.μ
 
-μ(x::MvNormal2) = x.μ
+Σ(x::MvNormal) = AᵀA(x.U)
 
-Σ(x::MvNormal2) = x.L * x.L'
+Base.length(x::MvNormal) = x.n
 
-Base.size(x::MvNormal2) = x.n
+Base.:+(d₁::MvNormal, d₂::MvNormal) = MvNormal(μ(d₁) + μ(d₂), Σ(d₁) + Σ(d₂))
 
-Base.:+(d₁::MvNormal2, d₂::MvNormal2) = MvNormal2(μ(d₁) + μ(d₂), Σ(d₁) + Σ(d₂))
+Base.:+(v::AbstractVector, d::MvNormal) = MvNormal(v + μ(d), d.U)
 
-Base.:+(v::AbstractVector, d::MvNormal2) = MvNormal2(v + μ(d), d.L)
+Base.:*(m::AbstractMatrix, d::MvNormal) = MvNormal(m * μ(d), AᵀA(d.U * m'))
 
-Base.:*(m::AbstractMatrix, d::MvNormal2) = MvNormal2(m * μ(d), m * d.L)
+Base.:(≈)(d₁::MvNormal, d₂::MvNormal) = (μ(d₁) ≈ μ(d₂)) && (Σ(d₁) ≈ Σ(d₂))
 
-Base.:(≈)(d₁::MvNormal2, d₂::MvNormal2) = (μ(d₁) ≈ μ(d₂)) && (Σ(d₁) ≈ Σ(d₂))
-
-function Base.:&(d₁::MvNormal2, d₂::MvNormal2)
-    c = cholesky(Σ(d₁) + Σ(d₂))
+function Base.:&(d₁::MvNormal, d₂::MvNormal)
+    c = cholesky!(Σ(d₁) + Σ(d₂))
     L⁻¹ = inv(c.L)
     Σ₁, Σ₂ = L⁻¹ * Σ(d₁), L⁻¹ * Σ(d₂)
     μ₁, μ₂ = L⁻¹ * μ(d₁), L⁻¹ * μ(d₂)
-    return MvNormal2(Σ₂'μ₁ + Σ₁'μ₂, Σ₁'Σ₂)
+    return MvNormal(Σ₂'μ₁ + Σ₁'μ₂, cholesky!(Σ₁'Σ₂).U)
 end
 
-function product(d₁::MvNormal2, ds...)
-    Σ = d₁.L |> Cholesky |> inv
-    μ = Σ * d₁.μ 
+function product(d₁::MvNormal, ds...)
+    if isempty(ds) return d₁ end
+    Λ = Cholesky(d₁.U, :U, 0) |> inv
+    μ = Λ * d₁.μ 
     for d in ds
-        Σᵢ = d₁.L |> Cholesky |> inv
-        μ .+= Σᵢ * d.μ
-        Σ .+= Σᵢ
+        Λᵢ = Cholesky(d.U, :U, 0) |> inv
+        μ .+= Λᵢ * d.μ
+        Λ .+= Λᵢ
     end
-    Σ⁻¹ = Σ |> cholesky |> inv
-    return MvNormal2(Σ⁻¹ * μ, cholesky(Σ⁻¹).L)
+    Σ = Λ |> Hermitian |> cholesky! |> inv
+    return MvNormal(Σ * μ, cholesky!(Σ).U)
 end
 
 # Logpdf
 
-function logpdf(d::MvNormal2{T,S}, x::AbstractVector) where {T,S}
-    ld = log(2π)*size(d) + 2*logdet(d.L)
-    le = d.L\(x .- d.μ)
+function Distributions.logpdf(d::MvNormal{T,S}, x::AbstractVector) where {T,S}
+    ld = log(2π)*length(d) + 2*logdet(d.U)
+    le = d.U'\(x .- d.μ)
     return -0.5*(ld + le'le)
 end
 
-function ChainRulesCore.rrule(::typeof(logpdf), d::MvNormal2{T,S}, x::AbstractVector) where {T,S}
-    z = x - μ(d)
-    cz = d.L\z
-    ld = log(2π)*size(d) + 2*logdet(d.L)
+function ChainRulesCore.rrule(::typeof(logpdf), d::MvNormal{T,S}, x::AbstractVector) where {T,S}
+    z = x .- d.μ
+    C = Cholesky(d.U, :U, 0)
+    L⁻¹z = d.U'\z
+    Σ⁻¹z = C\z
+    ld = log(2π)*length(d) + 2*logdet(d.U)
     logpdf_pb(Δy) = begin
-        Σₜ = @thunk begin
-            A = inv(c) .- cz .* cz'
-            ProjectTo(d.Σ)(-0.5 .* Δy .* (2A .- Diagonal(A)))
-        end
-        (NoTangent(), Tangent{MvNormal2}(μ = Δy .* cz, Σ = Σₜ), -Δy .* cz)
+        Σₜ = @thunk ProjectTo(d.U)(Δy .* (L⁻¹z * Σ⁻¹z' .- inv(Diagonal(d.U))))
+        (NoTangent(), Tangent{MvNormal}(n = ZeroTangent(), μ = Δy .* Σ⁻¹z, U = Σₜ), -Δy .* Σ⁻¹z)
     end
-    return -0.5*(ld + z'cz), logpdf_pb
+    return -0.5*(ld + AᵀA(L⁻¹z)), logpdf_pb
 end
 
-function logpdf(d::IsoMvNormal2, x::AbstractVector)
-    return -0.5*(log(2π)*size(d) + x'x)
+function Distributions.logpdf(d::IsoMvNormal, x::AbstractVector)
+    return -0.5*(log(2π)*length(d) + x'x)
 end
 
-function ChainRulesCore.rrule(::typeof(logpdf), d::IsoMvNormal2, x::AbstractVector)
-    isomvn_dll(s) =  (NoTangent(), ZeroTangent(), -s .* x)
+function ChainRulesCore.rrule(::typeof(logpdf), d::IsoMvNormal, x::AbstractVector)
+    isomvn_dll(Δy) = (NoTangent(), ZeroTangent(), -Δy .* x)
     return logpdf(d, x), isomvn_dll
 end
 
 # Logpdfnan
 
-function logpdfnan(d::MvNormal2{T,S}, x::AbstractVector, nums = .!isnan.(x)) where {T,S}
+function logpdfnan(d::MvNormal{T,S}, x::AbstractVector, nums = .!isnan.(x)) where {T,S}
     if !any(nums) return 0.0 end
     if all(nums) return logpdf(d, x) end
-    if S <: Diagonal return logpdf(MvNormal(μ(d)[nums], Diagonal(Σ(d).diag[nums])), x[nums]) end
-    return logpdf(MvNormal(μ(d)[nums], Σ(d)[nums, nums]), x[nums])
+    return logpdf(MvNormal(d.μ[nums], S(S <: Diagonal ? d.U.diag[nums] : d.U[nums,nums])), x[nums])
 end
 
-function ChainRulesCore.rrule(::typeof(logpdfnan), d::MvNormal2{T,S}, x::AbstractVector, nums = .!isnan.(x)) where {T,S} 
+function ChainRulesCore.rrule(::typeof(logpdfnan), d::MvNormal{T,S}, x::AbstractVector, nums = .!isnan.(x)) where {T,S} 
     if !any(nums) return 0.0, Δy -> NoTangent(), ZeroTangent(), ZeroTangent() end
     if all(nums) return rrule(logpdf, d, x) end
-    y, y_pb = rrule(logpdf, MvNormal(μ(d)[nums], S <: Diagonal ? Diagonal(Σ(d).diag[nums]) : Σ(d)[nums, nums]), x[nums])
+    y, y_pb = rrule(logpdf, MvNormal(d.μ[nums], S(S <: Diagonal ? d.U.diag[nums] : d.U[nums,nums])), x[nums])
     logpdfnan_pb(Δy) = begin
         _, Δd, Δx = y_pb(Δy)
-        μ₁ = @thunk begin μ₀ = zeros(size(μ(d))); view(μ₀,nums) .= Δd.μ; μ₀ end
-        Σ₁ = @thunk begin Σ₀ = zeros(size(Σ(d))); view(Σ₀,nums,nums) .= Δd.Σ; ProjectTo(Σ(d))(Σ₀) end
+        μ₁ = @thunk begin μ₀ = zeros(size(d.μ)); view(μ₀,nums) .= Δd.μ; μ₀ end
+        U₁ = @thunk begin U₀ = zeros(size(d.U)); view(U₀,nums,nums) .= Δd.U; ProjectTo(d.U)(U₀) end
         x₁ = @thunk begin x₀ = zeros(size(x)); view(x₀,nums) .= Δx; x₀ end
-        (NoTangent(), Tangent{MvNormal}(μ = μ₁, Σ = Σ₁), x₁)
+        (NoTangent(), Tangent{MvNormal}(n = NoTangent(), μ = μ₁, U = U₁), x₁)
     end
     return y, logpdfnan_pb
 end
 
 # Rand
 
-Base.:rand(d::MvNormal2, n::Int64) = [μ(x) + d.L * randn(size(d)) for _ in 1:n]
+Base.rand(d::MvNormal, n::Int64) = [rand(d) for _ in 1:n]
 
-Base.:rand(d::IsoMvNormal2, n::Int64) = [randn(size(d)) for _ in 1:n]
+Base.rand(d::MvNormal) = d.μ .+ d.U' * randn(length(d))
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using MvNormals
+import Distributions: MultivariateNormal
 using LinearAlgebra
 using Test
 
@@ -7,10 +8,31 @@ n = 2
 mvn₀ = MvNormal(n)
 mvn₁ = MvNormal(ones(n), Diagonal(ones(n)))
 mvn₂ = MvNormal(ones(n), Matrix(2.0I(n)))
+mvn₃ = MvNormal(ones(n), (x->x'x)(randn(n,n)))
 
-@testset "MvNormal" begin
+dvn₀ = MultivariateNormal(zeros(n), Matrix(1.0I(n)))
+dvn₁ = MultivariateNormal(ones(n), Diagonal(ones(n)))
+dvn₂ = MultivariateNormal(ones(n), Matrix(2.0I(n)))
+dvn₃ = MultivariateNormal(ones(n), (x->x'x)(mvn₃.U))
+
+nanvn₁ = MvNormal(ones(3), Diagonal(ones(3)))
+nanvn₂ = MvNormal(ones(3), Matrix(2.0I(3)))
+
+@testset "operators" begin
     @test mvn₀ + mvn₁ == mvn₂
-    @test ones(n) + Matrix(1.0I(n)) * mvn₀ == mvn₁
-    @test mvn₂ & mvn₂ == mvn₁
-    @test μ(mvn₁ & mvn₀) == Zero()
+    @test mvn₂ & mvn₂ ≈ mvn₁
+    @test product(mvn₁, mvn₁, mvn₁) isa MvNormal
+end
+
+@testset "logpdf" begin
+    @test logpdf(mvn₀, ones(n)) ≈ logpdf(dvn₀, ones(n))
+    @test logpdf(mvn₁, ones(n)) ≈ logpdf(dvn₁, ones(n))
+    @test logpdf(mvn₂, ones(n)) ≈ logpdf(dvn₂, ones(n))
+    @test logpdf(mvn₃, ones(n)) ≈ logpdf(dvn₃, ones(n))
+    @test logpdfnan(nanvn₁, [1.0, 1.0, NaN]) ≈ logpdf(mvn₁, ones(2))
+    @test logpdfnan(nanvn₂, [1.0, 1.0, NaN]) ≈ logpdf(mvn₂, ones(2))
+end
+
+@testset "rrules" begin
+    
 end
